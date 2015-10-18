06e722d8c9b9fc3267b6ff0300d9bbe882dddd3f
jinliang <jinliang@jinliangdeMacBook-Pro.local>
2015-10-19 07:28:12 +0800

    初始化项目的代码

---
+++ app/src/main/java/com/doudou/chat/medicineshoping/MainActivity.java
@@ -0,0 +1,28 @@
+package com.doudou.chat.medicineshoping;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.util.LogUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+public class MainActivity extends Activity {
+
+    @ViewInject(R.id.content)
+    private TextView content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
+        LogUtils.i("androidStudio测试");
+        content.setText("androidStudio测试");
+
+
+
+    }
+
+}
